87a51cc8e36fbb4b757a417bf1cf83385b5f2cdb
caiopo <caiopoliveira@gmail.com>
2016-03-06 04:52:32 -0300

    Improved group behavior

--- src/handler.py
+++ src/handler.py
@@ -10,7 +10,8 @@ except ImportError:
 	from PIL import Image
 
 help_text = """
-Send me an image and I will run Google's Tesseract OCR tool on it and send you back the results.
+Send me an image and I will run Google's Tesseract OCR tool on it \
+and send you back the results.
 
 You can change the language I use on Tesseract by sending me `/lang <code>`.
 
@@ -23,6 +24,13 @@ spa  -> Spanish
 fra  -> French
 ita  -> Italian
 jpn  -> Japanese
+
+In groups, I will not automatically parse the images, \
+I will wait for someoneto use the /tesseract command, \
+then I'll parse the last image sent.
+
+Made by caiopoliveira@gmail.com
+Source on https://github.com/caiopo/tesseract-bot
 """
 
 # /setcommands
@@ -33,6 +41,8 @@ DEFAULT_LANG = 'por'
 
 lang_dict = {}
 
+group_photos = {}
+
 available_langs = {'eng' : 'English',
 					'por' : 'Portuguese',
 					'spa' : 'Spanish',
@@ -41,50 +51,36 @@ available_langs = {'eng' : 'English',
 					'jpn' : 'Japanese',
 					}
 
-
 def start(bot, update):
-	bot.sendMessage(chat_id=update.message.chat_id, text="Hello! I'm Tesseract Bot!\n\n"+help_text, parse_mode=telegram.ParseMode.MARKDOWN)
+	bot.sendMessage(chat_id=update.message.chat_id,
+		text="Hello! I'm Tesseract Bot!\n\n"+help_text,
+		parse_mode=telegram.ParseMode.MARKDOWN,
+		disable_web_page_preview=True)
 
 def help(bot, update):
-	bot.sendMessage(chat_id=update.message.chat_id, text=help_text, parse_mode=telegram.ParseMode.MARKDOWN)
+	bot.sendMessage(chat_id=update.message.chat_id,
+		text=help_text,
+		parse_mode=telegram.ParseMode.MARKDOWN,
+		disable_web_page_preview=True)
 
 def unknown(bot, update):
-	bot.sendMessage(chat_id=update.message.chat_id, text="Sorry, I didn't understand that command.")
+	if update.message.chat_id > 0: # user	
+		bot.sendMessage(chat_id=update.message.chat_id,
+			text="Sorry, I didn't understand that command.")
 
 def message(bot, update):
-
-	print(update.message.chat_id)
-
 	if not update.message.photo:
 		return
-	
-	try:
-		photoFile = bot.getFile(update.message.photo[-1].file_id)
 
-		filename = config.CACHE_DIR+'/photo_'+''.join(str(time.time()).split('.'))+'.jpg'
+	photosize = bot.getFile(update.message.photo[-1].file_id)
 
-		photoFile.download(filename)
+	if update.message.chat_id > 0: # user	
 
-		language = lang_dict.get(update.message.chat_id, DEFAULT_LANG)
+		_photosize_to_parsed(bot, update, photosize)
 
-		image_text = pytesseract.image_to_string(Image.open(filename), language)
+	else: # group
 
-		if config.CACHE_TEMP:
-			os.remove(filename)
-
-		sanitized_string = _sanitize_string(image_text)
-
-		if sanitized_string:
-			response_msg = 'Parsed in {}:\n\n```\n{}\n```'.format(
-				available_langs[language], sanitized_string)
-		else:
-			response_msg = 'Nothing found! :(\nParsed in {}'.format(available_langs[language])
-
-		bot.sendMessage(chat_id=update.message.chat_id,
-						text=response_msg,
-						parse_mode=telegram.ParseMode.MARKDOWN)
-	except Exception as e:
-		_something_wrong(bot, update, e)
+		group_photos[update.message.chat_id] = photosize
 
 
 def lang(bot, update):
@@ -107,7 +103,38 @@ def lang(bot, update):
 
 
 def tesseract(bot, update):
-	pass
+	if update.message.chat_id > 0:
+		bot.sendMessage(chat_id=update.message.chat_id, text='/tesseract command is only available to groups.')
+	else:
+		_photosize_to_parsed(bot, update, group_photos[update.message.chat_id])
+
+
+def _photosize_to_parsed(bot, update, photosize):
+	try:
+		filename = config.CACHE_DIR+'/photo_'+''.join(str(time.time()).split('.'))+'.jpg'
+
+		photosize.download(filename)
+
+		language = lang_dict.get(update.message.chat_id, DEFAULT_LANG)
+
+		image_text = pytesseract.image_to_string(Image.open(filename), language)
+
+		if config.CACHE_TEMP:
+			os.remove(filename)
+
+		sanitized_string = _sanitize_string(image_text)
+
+		if sanitized_string:
+			response_msg = 'Parsed in {}:\n\n```\n{}\n```'.format(
+				available_langs[language], sanitized_string)
+		else:
+			response_msg = 'Nothing found! :(\nParsed in {}'.format(available_langs[language])
+
+		bot.sendMessage(chat_id=update.message.chat_id,
+						text=response_msg,
+					parse_mode=telegram.ParseMode.MARKDOWN)
+	except Exception as e:
+		_something_wrong(bot, update, e)
 
 def _something_wrong(bot, update, e):
 	bot.sendMessage(chat_id=update.message.chat_id, text='Something went wrong...\nError type: {}\nError message: {}'.format(type(e), e))
